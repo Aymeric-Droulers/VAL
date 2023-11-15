5a638ffbe30d60304a99f5d2de640b926fcb51a1
Aymeric-Droulers <112798634+Aymeric-Droulers@users.noreply.github.com>
2023-11-15 08:59:07 +0100

    Initial commit

---
+++ VAL-Code/VAL-Code/VAL-Code.cpp
@@ -0,0 +1,12 @@
+﻿// VAL-Code.cpp : définit le point d'entrée de l'application.
+//
+
+#include "VAL-Code.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
